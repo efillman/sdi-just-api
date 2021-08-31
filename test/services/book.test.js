260070b2d9a3dae16a936951f06413e97fec7d5f
Evan Fillman <efillman@gmail.com>
2021-08-31 17:35:31 -0400

    worked on tests

--- test/services/book.test.js
+++
@@ -1,8 +0,0 @@
-const app = require('../../src/app');
-
-describe('\'book\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('book');
-    expect(service).toBeTruthy();
-  });
-});
--- test/services/system.test.js
+++ test/services/system.test.js
@@ -1,8 +1,37 @@
+const axios = require('axios');
+const url = require('url');
 const app = require('../../src/app');
 
+const port = app.get('port') || 8998;
+const getUrl = pathname => url.format({
+  hostname: app.get('host') || 'localhost',
+  protocol: 'http',
+  port,
+  pathname
+});
+
 describe('\'system\' service', () => {
+  let server;
+
+  beforeAll(done => {
+    server = app.listen(port);
+    server.once('listening', () => done());
+  });
+
+  afterAll(done => {
+    server.close(done);
+  });
+
   it('registered the service', () => {
     const service = app.service('system');
     expect(service).toBeTruthy();
+  });
+
+  it('starts and returns the system endpoint', async () => {
+    expect.assertions(1);
+
+    const { data } = await axios.get(getUrl('system'));
+
+    expect(data['limit']).toBe(10);
   });
 });
