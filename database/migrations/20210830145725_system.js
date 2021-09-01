93ea8e8f4affaf884cd7d62fa5fcfa6c17f7ebb6
young <badfriend87@hotmail.com>
2021-09-01 14:40:50 -0700

    making more seed

--- database/migrations/20210830145725_system.js
+++ database/migrations/20210830145725_system.js
@@ -1,17 +1,17 @@
 
-exports.up = function(knex) {
-    return knex.schema.createTable('system', (table) => {
-      table.increments('system_id').primary();
-      table.integer('service_id');
-      table.string('system_short_name').notNullable();
-      table.string('system_long_name').notNullable();
-      table.integer('system_importance').defaultTo(0).notNullable();
-      table.string('system_note');
-      table.foreign('service_id').references('services.service_id');
-      table.timestamps(true, true);
-    });
-  };
+exports.up = function (knex) {
+  return knex.schema.createTable('system', (table) => {
+    table.increments('system_id').primary();
+    table.integer('service_id');
+    table.string('system_short_name').notNullable();
+    table.string('system_long_name').notNullable();
+    table.integer('system_importance').defaultTo(0).notNullable();
+    table.string('system_note');
+    table.foreign('service_id').references('services.service_id');
+    table.timestamps(true, true);
+  });
+};
 
-  exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('system');
-  };
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('system');
+};
